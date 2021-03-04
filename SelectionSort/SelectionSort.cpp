0f793265fae3ad5bdc64e825aa85dc42a665f449
jianye0428 <jianye0428@gmail.com>
2021-03-04 21:45:28 +0100

    'SelectionSort'

---
+++ SelectionSort/SelectionSort.cpp
@@ -0,0 +1,13 @@
+
+void SelectionSort(vector<int> &a){
+    int len = a.size();
+    for(int i=0, minIndex; i<len-1; i++){
+        minIndex = i;
+        for(int j=i+1; j<len; j++){
+            if(a[j]<a[minIndex]){
+                minIndex = j;
+            }
+        }
+        swap(a[i],a[minIndex]);
+    }
+}
