64467842840e695d1d21e8069b0ccf70a941263e
jianye0428 <jianye0428@gmail.com>
2021-03-05 15:06:52 +0100

    'InsertionSort'

---
+++ InsertionSort/InsertionSort.cpp
@@ -0,0 +1,12 @@
+void insertionsort(vector<int> &a){
+    int len = a.size();
+    for(int i=0, j, temp; i<len-1; i++){//需要循环次数
+        j = i;
+        temp = a[i+1]; 
+        while(j>=0 && a[j]>temp){
+            a[j+1]=a[j];
+            j--;
+        }
+        a[j+1]=temp;
+    }
+}
